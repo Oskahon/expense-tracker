f85c5423c276c864697b96ad60bf8159c6e54b8a
Oskari Ahonen <ahonenoskari@gmail.com>
2025-02-24 19:34:49 +0200

    Add index.js and basic plan for starting work

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Welcome to using the Expense Tracker');
+
+// 1. Look into commander.js library
+// 2. Create a list of command and what they are supposed to do
+// 3. Start planning what functionality is required for the app
+//      Some ideas to get started:
+//      - Get commandline arguments
+//      - Save data to file in JSON
+//      - Parse flags and data from arguments
+//      - Create functionality for the commands
+//      - Create a help functionality that explains the app usage
