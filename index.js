81642d3ef6796d0e3023c64b45503a986b7255c4
Oskari Ahonen <ahonenoskari@gmail.com>
2025-02-25 06:56:48 +0200

    Additional planning

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 console.log('Welcome to using the Expense Tracker');
 
+//** The Plan */
 // 1. Look into commander.js library
-// 2. Create a list of command and what they are supposed to do
+//// 2. Create a list of command and what they are supposed to do
 // 3. Start planning what functionality is required for the app
 //      Some ideas to get started:
 //      - Get commandline arguments
@@ -9,3 +10,16 @@ console.log('Welcome to using the Expense Tracker');
 //      - Parse flags and data from arguments
 //      - Create functionality for the commands
 //      - Create a help functionality that explains the app usage
+
+//** Commands */
+// add - Add an expense
+// update - Update an expense
+// delete - Detele an expense
+// list - List all expenses
+// summary - Get a summary of all expenses
+// summary -Month <month_number> - get a monthly summary of expenses
+
+//** Additional features */
+// Expense categories - Add categories to expenses for filtering
+// Set a monthly budget and show warning when nearing it
+// Allow exporting to CSV file
